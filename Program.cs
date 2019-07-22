1a9cd6a07595016f3ef88d61f70791b67fa22095
GabrielSousa <gabrielzinhosenac@gmail.com>
2019-07-22 09:16:42 -0300

    Realizando Cálculos

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Tabuada
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero;
+            //Limpar tela de console
+            Console.Clear();
+            Console.WriteLine("#############################");
+            Console.WriteLine("#            Programa de Tabuada         #");
+            Console.WriteLine();
+            Console.WriteLine("Digite um número > ");
+            numero = int.Parse(Console.ReadLine());
+
+
+            for(int x = 0; x <= 10; x++){
+                Console.WriteLine(numero+" x "+x+" = "+numero*x);
+            }
         }
     }
 }
